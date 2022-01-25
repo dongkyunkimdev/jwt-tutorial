57b3685f92e61ada49c7c43580b4401d5d53cedf
dongkyunkimdev <ddkds66@gmail.com>
2022-01-25 14:46:30 +0900

    initial commit

---
+++ src/test/java/kdk/jwttutorial/JwtTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package kdk.jwttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
