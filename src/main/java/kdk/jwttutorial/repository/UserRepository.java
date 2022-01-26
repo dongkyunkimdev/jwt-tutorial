bc5f72ddfdca4e9ad04ce9b33858be0f7f764bda
dongkyunkimdev <ddkds66@gmail.com>
2022-01-26 13:54:06 +0900

    feat: UserDetailsService 구현
    loadUserByUsername Override를 위해 UserRepository, UserDetailsService 및 AuthController 구현

---
+++ src/main/java/kdk/jwttutorial/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package kdk.jwttutorial.repository;
+
+import java.util.Optional;
+import kdk.jwttutorial.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	// Eager 조회로 authorities 정보를 함께 가져옴
+	@EntityGraph(attributePaths = "authorities")
+	Optional<User> findOneWithAuthoritiesByUsername(String username);
+}
