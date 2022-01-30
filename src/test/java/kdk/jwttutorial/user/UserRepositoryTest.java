04372d5bd14f833df309406df14a1c8d080cad9e
dongkyunkimdev <ddkds66@gmail.com>
2022-01-30 18:54:57 +0900

    test: UserRepository Test 추가
    Spring Data JPA를 사용하고 있기 때문에 JPA 관련 컴포넌트들만 로드하는 @DataJpaTest를 통해 @SpringBootTest보다 가벼운 테스트를 진행함
    관련하여 SecurityConfig에서 Bean으로 등록하는 BCryptPasswordEncoder같은 경우 @DataJpaTest 사용시 빈으로 등록이 안되어 의존성 주입이 불가능하기 때문에 직접 인스턴스를 생성하여 구현함

---
+++ src/test/java/kdk/jwttutorial/user/UserRepositoryTest.java
@@ -0,0 +1,98 @@
+package kdk.jwttutorial.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+import kdk.jwttutorial.security.auth.Authority;
+import kdk.jwttutorial.user.dto.UserDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class UserRepositoryTest {
+
+	@Autowired
+	private UserRepository userRepository;
+	private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+	@Test
+	void 회원가입() {
+		// given
+		User user = createUser("test1@test.com", "password", "test1");
+
+		// when
+		User result = userRepository.save(user);
+
+		// then
+		assertThat(user).isSameAs(result);
+		assertThat(user.getEmail()).isEqualTo(result.getEmail());
+		assertThat(user.getPassword()).isEqualTo(result.getPassword());
+		assertThat(user.getNickname()).isEqualTo(result.getNickname());
+		assertThat(user.getUserId()).isEqualTo(result.getUserId());
+		assertThat(user.getAuthorities()).isEqualTo(result.getAuthorities());
+
+	}
+
+	@Test
+	void 사용자_조회() {
+		// given
+		User user = createUser("test1@test.com", "password", "test1");
+		userRepository.save(user);
+
+		// when
+		User result = userRepository.findOneWithAuthoritiesByEmail(user.getEmail()).orElse(null);
+
+		// then
+		assertThat(user).isSameAs(result);
+		assertThat(user.getEmail()).isEqualTo(result.getEmail());
+		assertThat(user.getPassword()).isEqualTo(result.getPassword());
+		assertThat(user.getNickname()).isEqualTo(result.getNickname());
+		assertThat(user.getUserId()).isEqualTo(result.getUserId());
+		assertThat(user.getAuthorities()).isEqualTo(result.getAuthorities());
+	}
+
+	@Test
+	void 사용자가_존재함() {
+		// given
+		User user = createUser("test1@test.com", "password", "test1");
+		userRepository.save(user);
+
+		// when
+		boolean result = userRepository.existsByEmail(user.getEmail());
+
+		// then
+		assertThat(true).isSameAs(result);
+	}
+
+	@Test
+	void 사용자가_존재하지_않음() {
+		// given
+		User user = createUser("test1@test.com", "password", "test1");
+
+		// when
+		boolean result = userRepository.existsByEmail(user.getEmail());
+
+		// then
+		assertThat(false).isSameAs(result);
+	}
+
+	private User createUser(String email, String password, String nickname) {
+		UserDto userDto = UserDto.builder()
+			.email(email)
+			.password(password)
+			.nickname(nickname).build();
+
+		User user = User.builder()
+			.email(userDto.getEmail())
+			.password(passwordEncoder.encode(userDto.getPassword()))
+			.nickname(userDto.getNickname())
+			.authorities(Collections.singleton(Authority.createUserRole()))
+			.build();
+		return user;
+	}
+}
